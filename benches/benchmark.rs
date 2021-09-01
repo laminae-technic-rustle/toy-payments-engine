658aa564edfc2ef5e73c81255283be21e1d1de04
laminae-technic-rustle <oizys-curarum@protonmail.com>
2021-09-01 23:43:46 +0200

    :zap: - Change bench file name

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lib::bench;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("main 20", |b| b.iter(|| bench("transactions.csv")));
+    c.bench_function("bench 20", |b| b.iter(|| bench("bench.csv")));
 }
 
 criterion_group!(benches, criterion_benchmark);
